aab62e76d325fda4f6c43248684a7fd1fdf3d8ec
skycyp <1015729701@qq.com>
2020-05-28 18:46:08 +0800

    new commit

--- instapicture/src/pages/home/index/contentPage/friendListItem.js
+++ instapicture/src/pages/home/index/contentPage/friendListItem.js
@@ -2,12 +2,30 @@ import React, { Component, PropTypes } from 'react';
 import {Link} from 'react-router-dom';
 import styles from '../css/ListItemStyle.css' ;
 import config from '../../../../assets/js/conf/config.js';
+import {request} from '../../../../assets/js/libs/request.js';
+import {safeAuth,lazyImg} from '../../../../assets/js/utils/util.js';
+import Css2 from '../css/friendsList.css';
 
 export default class FriendListItem extends Component {
     constructor(props) {
       super(props);
+      this.state={
+        sInterest:[]
+      }
     }
+    componentDidMount(){
+      this.getReco();
+    }
+    getReco(){ 
+      request(config.proxyBaseUrl+"/api/userinfos/queryinfo?token="+config.token,"post",{uid:this.props.itemData.UserID}).then(res=>{
+        if (res.code ===200){
+            this.setState({sInterest:res.data.tags},()=>{
+                lazyImg();
+            })
+        }
+    } );
   
+    }
     render() {
       let datas=this.props.itemData;
         return(
@@ -16,6 +34,13 @@ export default class FriendListItem extends Component {
               <a><img src={require("../../../../assets/images/common/lazyImg.jpg")} data-echo={datas.ProfileImagePath} className={styles.imgStyle}></img></a>
               <a className={styles.wid}>{datas.UserName}</a>
               <span><Link to={{pathname:config.path+"home/index/personalpage/index",state:datas.UserID}}>主页</Link></span>
+               <div className={Css2["alert"]} >
+                    <label>
+                        {this.state.sInterest.map((tag,index) =>
+                            (<span className={Css2["badge"]} key={index}>{tag}</span>)
+                        )}
+                        </label>
+                </div>
             </div>
           </div>
   
